7c6ee4f8f6d984bc8e9883420c7a5e6d49e2ba40
Ankit Agarwal <agarwalankit95@gmail.com>
2022-01-08 21:57:54 +0530

    feat: Initial react setup

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Symbol</th>
+          <th>Description</th>
+          <th>Underlying Asset</th>
+          <th>Market Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Test1</td>
+          <td>Test2</td>
+          <td>Test3</td>
+          <td>Test4</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+export default App;
